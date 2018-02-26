d529456a17117b99c4782087768d45716fd311c6
Administrator <jiaojinda@163.COM>
2018-02-26 15:40:33 +0800

    SpringBootREST upload

---
+++ src/main/java/com/jiao/PersonRepository.java
@@ -0,0 +1,15 @@
+package com.jiao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/2/26.
+ */
+public interface PersonRepository extends JpaRepository<Person,Long> {
+    @RestResource(path = "nameStartsWith",rel = "nameStartsWith")
+    List<Person> findByNameStartsWith(@Param("name") String name);
+}
